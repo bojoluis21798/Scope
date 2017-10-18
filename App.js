00a3801a5997fa0a7916fa696c490699736dc3f7
bojoluis21798 <trinity21798@gmail.com>
2017-10-18 16:45:34 +0800

    Added Title

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ class LogIn extends Component {
     return(
       <View style={styles.container}>
         <View style={styles.title}>
+          <Text style={styles.titleText}>
+            WELCOME
+          </Text>
         </View>
       </View>
     );
@@ -14,10 +17,20 @@ class LogIn extends Component {
 }
 
 export default class App extends Component {
-  componentDidMount(){
-    Font.loadAsync({
+  constructor(props){
+    super(props);
+    this.state = {
+      fontLoaded: false,
+    }
+  }
+
+  async componentDidMount(){
+    await Font.loadAsync({
       'montserrat-reg': require('./Fonts/Montserrat-Regular.ttf'),
+      'montserrat-extra-bold': require('./Fonts/Montserrat-ExtraBold.ttf'),
     });
+
+    this.setState({fontLoaded: true});
   }
   render() {
     return (
@@ -30,14 +43,22 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#73D74B',
-    justifyContent: 'center',
+    backgroundColor: 'white',
     alignItems: 'center',
   },
   title: {
-    height: 400,
-    width: 360,
-    borderColor: 'white',
-    borderWidth: 5,
+    borderBottomLeftRadius: 400,
+    borderBottomRightRadius: 400,
+    height: 300,
+    width: 500,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green',
   },
+  titleText: {
+    fontFamily: 'montserrat-extra-bold',
+    fontWeight: 'bold',
+    fontSize: 50,
+    color: 'white',
+  }
 });
