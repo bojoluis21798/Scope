2684b778bf9bf08c82a5d8732e1dbfea42ccc343
bojoluis21798 <trinity21798@gmail.com>
2017-10-18 16:09:27 +0800

    Link Font

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Font} from 'expo';
 
-export default class App extends React.Component {
-  render() {
-    return (
+class LogIn extends Component {
+  render(){
+    return(
       <View style={styles.container}>
         <View style={styles.title}>
-          
         </View>
       </View>
     );
   }
 }
+
+export default class App extends Component {
+  componentDidMount(){
+    Font.loadAsync({
+      'montserrat-reg': require('./Fonts/Montserrat-Regular.ttf'),
+    });
+  }
+  render() {
+    return (
+      <LogIn />
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -25,5 +39,5 @@ const styles = StyleSheet.create({
     width: 360,
     borderColor: 'white',
     borderWidth: 5,
-  }
+  },
 });
