4d0a4e492b25209ca04e877b1541b76e9bb2ff27
bojoluis21798 <trinity21798@gmail.com>
2017-10-18 15:22:12 +0800

    Add Background

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.title}>
+          
+        </View>
       </View>
     );
   }
@@ -16,8 +16,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#73D74B',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  title: {
+    height: 400,
+    width: 360,
+    borderColor: 'white',
+    borderWidth: 5,
+  }
 });
