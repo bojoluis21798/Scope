07174913531f8861c3858d158dd93484802b71fe
bojoluis21798 <trinity21798@gmail.com>
2017-11-09 16:10:45 +0800

    Add NewProduct

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import {LockStack, InitialStack} from './Assets/InitialStack.js';
-import {Product} from './Assets/Screens/Product.js'; //test
+import {NewProduct} from './Assets/Screens/NewProduct.js'; //test
 
 export default class App extends Component {
   render() {
     return (
-      <Product />
+      <NewProduct />
     );
   }
 }
